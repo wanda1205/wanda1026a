e89981a2e834f5fbc5c47a6ba79d8704a01fd2e3
wanda1205a <aa0925653552@gmail.com>
2023-11-16 10:07:07 +0800

    make it better

--- index.py
+++ index.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-	X = "作者:陳羿汶!2023-11-09a<br>"
+	X = "作者:陳羿汶!2023-11-16a<br>"
 	X += "<a href=/mis>資訊導論</a><br>"
 	X += "<a href=/today>日期時間</a><br>"
 	X += "<a href=/about>羿汶的網頁</a><br>"
