0cc19a7a56445ccea9630f21b7bb873fc6eb756d
wanda1205 <aa0925653552@gmail.com>
2023-12-08 11:05:42 +0800

    make it better

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ from firebase_admin import credentials, firestore
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 
-from flask import Flask,render_template, request
+from flask import Flask,render_template, request,make_response, jsonify
 from datetime import datetime
 
 import requests
@@ -17,7 +17,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-	X = "作者:陳羿汶!2023-11-30a<br>"
+	X = "作者:陳羿汶!2023-12-07a<br>"
 	X += "<a href=/mis>資訊導論</a><br>"
 	X += "<a href=/today>日期時間</a><br>"
 	X += "<a href=/about>羿汶的網頁</a><br>"
@@ -28,7 +28,8 @@ def index():
 	X += "<a href=/search>根據書名關鍵字查詢圖書</a><br><br>"
 	X += "<a href=/spider>網路爬蟲擷取子青老師課程資料</a><br><br>"
 	X += "<a href=/movie>讀取開眼電影即將上映影片，寫入Firestore</a><br>"
-	X += "<a href=/searchQ>查詢開眼電影即將上映影片</a><br>"
+	X += "<a href=/searchQ>查詢開眼電影即將上映影片</a><br><br>"
+	X += "<a href=/movie_rate>讀取開眼電影即將上映影片(含分級資訊)，寫入Firestore</a><br>"
 	return X
 
 @app.route("/mis")
@@ -188,15 +189,74 @@ def searchQ():
     else:  
         return render_template("input.html")
 
-# "書名：<a href=" + bk["url"] + ">" + bk["title"] + "</a><br>"
-# "片名：<a href=" + doc.to_dict()["title"] + "</a><br>"
-# bk = doc.to_dict()
-# 			if keyword in bk["title"]:
-# 				Result += "書名：<a href=" + bk["url"] + ">" + bk["title"] + "</a><br>"
-# 				Result += "作者：" + bk["author"] + "<br>"
-# 				Result += str(bk["anniversary"]) + "週年紀念版 " + "<br>"
-# 				Result += "<img src = " + bk["cover"] + "></img><br><br>"
-# 		return Result
+@app.route("/movie_rate")
+def movie_rate():
+    url = "http://www.atmovies.com.tw/movie/next/"
+    Data = requests.get(url)
+    Data.encoding = "utf-8"
+    sp = BeautifulSoup(Data.text, "html.parser")
+    result=sp.select(".filmListAllX li")
+    lastUpdate = sp.find(class_="smaller09").text[5:]
+
+    for x in result:
+        picture = x.find("img").get("src").replace(" ", "")
+        title = x.find("img").get("alt")    
+        movie_id = x.find("div", class_="filmtitle").find("a").get("href").replace("/", "").replace("movie", "")
+        hyperlink = "http://www.atmovies.com.tw" + x.find("a").get("href")
+
+        t = x.find(class_="runtime").text
+        showDate = t[5:15]
+
+        showLength = ""
+        if "片長" in t:
+            t1 = t.find("片長")
+            t2 = t.find("分")
+            showLength = t[t1+3:t2]
+
+        r = x.find(class_="runtime").find("img")
+        rate = ""
+        if r != None:
+            rr = r.get("src").replace("/images/cer_", "").replace(".gif", "")
+            if rr == "G":
+                rate = "普遍級"
+            elif rr == "P":
+                rate = "保護級"
+            elif rr == "F2":
+                rate = "輔12級"
+            elif rr == "F5":
+                rate = "輔15級"
+            else:
+                rate = "限制級"
+
+        doc = {
+            "title": title,
+            "picture": picture,
+            "hyperlink": hyperlink,
+            "showDate": showDate,
+            "showLength": showLength,
+            "rate": rate,
+            "lastUpdate": lastUpdate
+        }
+
+        db = firestore.client()
+        doc_ref = db.collection("電影含分級").document(movie_id)
+        doc_ref.set(doc)
+    return "近期上映電影已爬蟲及存檔完畢，網站最近更新日期為：" + lastUpdate
+
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    # build a request object
+    req = request.get_json(force=True)
+    # fetch queryResult from json
+    action =  req.get("queryResult").get("action")
+    msg =  req.get("queryResult").get("queryText")
+    info = "動作：" + action + "； 查詢內容：" + msg
+    return make_response(jsonify({"fulfillmentText": info}))
+
+
+
+
 
 if __name__ == "__main__":
 	app.run()
